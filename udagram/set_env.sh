ea97de1254d884568a12e6abe981c3a2df3294dc
minhhl <leminhdut108>
2023-11-24 15:06:47 +0700

    final

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=udagramdb.cpmbomf1vmkr.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=hocmai1234
+export POSTGRES_HOST=database-1.ck3uod918mcw.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::minhhl2-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=hello
+export JWT_SECRET=minhhl2_secret
 export URL=http://localhost:8080
